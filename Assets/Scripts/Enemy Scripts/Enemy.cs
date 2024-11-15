626815ca1d2b9f33cd7d304b4d781a4df096ea0e
AnviIsScared2 <islamanvi@gmail.com>
2024-11-14 20:34:21 -0500

    added in audio sources and implemented an audio manager

---
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator enemyAnim;
+    public Transform playerTrans;
+
+    [SerializeField] private AudioClip goblin_hit;
+
+    [SerializeField] private int health;
+
+    private string state;
+    private int moveSpeed = 3;
+    private int awarenessDist = 40;
+    private int attackDist = 15;
+    private bool attacking = false;
+    private float attackCooldown = 1.5f;
+
+    [SerializeField] private Attack fireballAttack;
+
+    void Start()
+    {
+        state = "idle";
+        //health = 2;
+        enemyAnim = this.GetComponent<Animator>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (!attacking)
+        {
+            if (Vector3.Distance(transform.position, playerTrans.position) <= awarenessDist)
+            {
+                if (state == "idle" || state == "attack")
+                {
+                    state = "chase";
+                    enemyAnim.Play("RigGob1_Run");
+                }
+            }
+
+            if (Vector3.Distance(transform.position, playerTrans.position) > awarenessDist)
+            {
+                if (state == "chase" || state == "attack")
+                {
+                    state = "idle";
+                    enemyAnim.Play("RigGob1_Idle");
+                }
+            }
+
+            if (Vector3.Distance(transform.position, playerTrans.position) <= attackDist && state == "chase")
+            {
+                state = "attack";
+                enemyAnim.Play("RigGob1_Idle");
+            }
+
+            if (state == "attack")
+            {
+                StartCoroutine(AttackPlayer());
+            }
+
+            if (state == "chase")
+            {
+                transform.LookAt(playerTrans);
+
+                if (Vector3.Distance(transform.position, playerTrans.position) >= attackDist)
+                {
+                    transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        attacking = true;
+
+        StartCoroutine(fireballAttack.Cast(transform));
+
+        yield return new WaitForSeconds(fireballAttack.CooldownTime());
+        attacking = false;
+    }
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            // TODO: Fix with actual damage of projectile
+            decreaseHealth(1);
+        }
+    }
+
+    public void decreaseHealth(int damage)
+    {
+        Sound_Effects.Instance.Play_Sound_Effects(goblin_hit, transform, 1f);
+        health -= damage;
+
+        if (health <= 0)
+        {
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
